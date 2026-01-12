1fda33d64dd7c5f33d3d330553c7d8a57dd914fd
Krzysztof Lipka <klipka1991@gmail.com>
2026-01-12 13:42:47 +0100

    Moving constants and types to separate files

---
+++ src/types/military.ts
@@ -0,0 +1,15 @@
+export type MilitaryType =
+    | "barracks"
+    | "naval_base"
+    | "airfield"
+    | "training_area"
+    | "yes"
+    | "range"
+    | "primary"
+    | "office"
+    | "danger_area"
+    | "checkpoint"
+    | "shelter"
+    | "bunker";
+
+export type GeoJSONData = GeoJSON.FeatureCollection;
